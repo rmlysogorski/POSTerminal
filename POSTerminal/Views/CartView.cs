2e4c49a773f2b839a5991e348106d6aa03fcaf2f
jeff_ducat <jducat81@gmail.com>
2019-05-09 17:33:12 -0400

    Added Imessage and deleted cc views

--- POSTerminal/Views/CartView.cs
+++ POSTerminal/Views/CartView.cs
@@ -19,7 +19,14 @@ namespace POSTerminal
 
         public void Display()
         {
-            
+            IView orderListView = new OrderListView(myOrder);
+            Console.WriteLine("Current Order: ");
+            orderListView.Display();
+            Console.WriteLine("1: Add an Item");
+            Console.WriteLine("2: Remove an Item");
+            Console.WriteLine("3: Cancel Order");
+            Console.WriteLine("4: Proceed to Checkout");
+
         }
     }
 }
--- POSTerminal/Views/OrderListView.cs
+++ POSTerminal/Views/OrderListView.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace POSTerminal.Views
+namespace POSTerminal
 {
-    class OrderListView
+    class OrderListView :IView
     {
         private Order myOrder;
 
