fba7b0501580cbe56470ce11adf7806b72f938d7
jeff_ducat <jducat81@gmail.com>
2019-05-10 14:10:00 -0400

    Added ReceiptView

--- POSTerminal/Views/ReceiptView.cs
+++ POSTerminal/Views/ReceiptView.cs
@@ -18,7 +18,43 @@ namespace POSTerminal
 
         public void Display()
         {
-            throw new NotImplementedException();
+            if (myOrder.PayInfo.PayType == "cash")
+            {
+                Console.Write($"RSJ Coffee House\t\tOrder Number: {myOrder.OrderNumber}");
+                Console.WriteLine($"\t\tDate: {DateTime.Now.ToString("yyyyMMddHHmmss")}");
+                Console.WriteLine($"{myOrder.PurchaseList}");
+                Console.WriteLine($"\t\tSubtotal: {myOrder.Subtotal}");
+                Console.WriteLine($"\t\tTax: {myOrder.Tax}");
+                Console.WriteLine($"\t\tTotal:{myOrder.Total}");
+                Console.WriteLine($"\t\tAmount Tendered: {myOrder.PayInfo.AmountTendered}");
+                Console.WriteLine($"\t\tChange: {myOrder.PayInfo.Change}");
+                Console.WriteLine($"Thank you for shopping at RSJ Coffee House");
+            }
+            else if (myOrder.PayInfo.PayType == "credit card")
+            {
+                Console.Write($"RSJ Coffee House\t\tOrder Number: {myOrder.OrderNumber}");
+                Console.WriteLine($"\t\tDate: {DateTime.Now.ToString("yyyyMMddHHmmss")}");
+                Console.WriteLine($"{myOrder.PurchaseList}");
+                Console.WriteLine($"\t\tSubtotal: {myOrder.Subtotal}");
+                Console.WriteLine($"\t\tTax: {myOrder.Tax}");
+                Console.WriteLine($"\t\tTotal:{myOrder.Total}");
+                Console.Write($"\tCard Number: {myOrder.PayInfo.CardNumber}");
+                Console.WriteLine($"\t\tAmount Tendered: {myOrder.PayInfo.AmountTendered}");
+                Console.WriteLine($"\t\tCash Back: {myOrder.PayInfo.CashBack}");
+                Console.WriteLine($"Thank you for shopping at RSJ Coffee House");
+            }
+            else
+            {
+                Console.Write($"RSJ Coffee House\t\tOrder Number: {myOrder.OrderNumber}");
+                Console.WriteLine($"\t\tDate: {DateTime.Now.ToString("yyyyMMddHHmmss")}");
+                Console.WriteLine($"\t{myOrder.PurchaseList}");
+                Console.WriteLine($"\t\tSubtotal: {myOrder.Subtotal}");
+                Console.WriteLine($"\t\tTax: {myOrder.Tax}");
+                Console.WriteLine($"\t\tTotal: {myOrder.Total}");
+                Console.Write($"\tCheck Number: {myOrder.PayInfo.CheckNumber}");
+                Console.WriteLine($"\t\tAmount Tendered: {myOrder.PayInfo.AmountTendered}");
+                Console.WriteLine($"Thank you for shopping at RSJ Coffee House");
+            }
         }
     }
 }
